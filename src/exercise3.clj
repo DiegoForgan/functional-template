bc451fb18a2039afee018e13b7f7b661ae346939
DiegoForgan <dmforgan89@gmail.com>
2017-08-21 21:33:58 -0300

    Exercises 3 and 4 DONE

--- src/exercise3.clj
+++ src/exercise3.clj
@@ -1,1 +1,10 @@
 (ns exercise3)
+
+(defn fibonacci
+  "Calculates the nth number in the Fibonacci series."
+  [n]
+  (if
+    (<= n 1)
+    (if (= n 0) 0 1)
+    (+ (fibonacci(- n 1)) (fibonacci (- n 2)))
+  ))
--- src/exercise4.clj
+++ src/exercise4.clj
@@ -1,1 +1,6 @@
 (ns exercise4)
+
+(defn summary
+  "Calculates the sum of all the elements on a list."
+  [elements]
+  (reduce + 0 elements))
